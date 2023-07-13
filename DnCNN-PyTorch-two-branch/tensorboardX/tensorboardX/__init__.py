2023d324e3f82a3ff72e1b0f3c6a1ccee8e78612
ZZZZZZZZJ <763017207@qq.com>
2023-07-13 23:15:59 +0800

    Add DnCNN

---
+++ DnCNN-PyTorch-two-branch/tensorboardX/tensorboardX/__init__.py
@@ -0,0 +1,18 @@
+"""A module for visualization with tensorboard
+"""
+
+from .record_writer import RecordWriter
+from .torchvis import TorchVis
+from .writer import FileWriter, SummaryWriter
+
+__version__ = "1.8"  # will be overwritten if run setup.py
+
+__version__ = "1.8+366bc8f"
+
+__version__ = "1.8+366bc8f"
+
+__version__ = "1.8+366bc8f"
+
+__version__ = "1.8+366bc8f"
+
+__version__ = "1.8+366bc8f"
